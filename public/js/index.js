2c18929f1c4f2535cbbca05b175b5a7c600ad810
nizk12 <nirmalkhawas02@gmail.com>
2025-01-09 11:03:47 +0000

    Up to date

--- public/js/index.js
+++ public/js/index.js
@@ -30,7 +30,7 @@ contactForm.addEventListener("submit", (e) => {
   };
 
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/");
+  xhr.open("POST", "https://vglory-backend.onrender.com");
   xhr.setRequestHeader("content-type", "application/json");
   xhr.onload = function () {
     console.log(xhr.responseText);
